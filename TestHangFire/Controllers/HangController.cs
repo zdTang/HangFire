bc3e1544fc6091205568d8ce90e66c49aa4a1abc
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-18 05:14:15 -0400

    4-2 Fire and Forget --- job done
    Use Postman to call the API. do not launch the Swagger which will cause issue.

--- TestHangFire/Controllers/HangController.cs
+++ TestHangFire/Controllers/HangController.cs
@@ -1,29 +1,31 @@
+using System.Linq;
+using System.Threading.Tasks;
 using Hangfire;
+using Hangfire.Server;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Extensions.Logging;
 namespace TestHangFire.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class HangController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get()
-        {
-            return Ok("Hello from hangfire web api !");
-        }
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+
 
         [HttpPost]
         [Route("[action]")]
-        public IActionResult Welcome() 
+        public IActionResult Welcome()
         {
-            BackgroundJob.Enqueue(()=>SendWelcomeEmail("Welcome to our app!"));
-            return Ok("Welcome.")
+            BackgroundJob.Enqueue(() => SendWelcomeEmail("Welcome to our app!",null));
+            return Ok(" Welcome.");
         }
 
-        private void SendWelcomeEmail(string email) 
-        { 
+        public void SendWelcomeEmail(string email, PerformContext context) 
+        {
+            string jobId = context.BackgroundJob.Id;
             Console.WriteLine(email);
+            Console.WriteLine($"Job ID:{jobId}, Welcome.");
         }
-    }
+    } 
 }
--- TestHangFire/Program.cs
+++ TestHangFire/Program.cs
@@ -22,11 +22,11 @@ namespace TestHangFire
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
 
             app.UseHttpsRedirection();
 
